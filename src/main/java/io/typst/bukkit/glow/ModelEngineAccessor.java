58f985c02523ac9103680b3b1cb4e6fb4168166b
Junhyung Im <entrypointkr@gmail.com>
2024-05-03 19:11:54 +0900

    fix: remove legacy method

--- src/main/java/io/typst/bukkit/glow/ModelEngineAccessor.java
+++ src/main/java/io/typst/bukkit/glow/ModelEngineAccessor.java
@@ -7,12 +7,9 @@ import com.ticxo.modelengine.api.model.bone.render.renderer.RenderQueues;
 import com.ticxo.modelengine.api.model.render.DisplayRenderer;
 import com.ticxo.modelengine.api.model.render.ModelRenderer;
 import com.ticxo.modelengine.core.model.render.DisplayRendererImpl;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.EntityType;
 
 import java.util.*;
-
-import static io.typst.bukkit.glow.ColorConverter.getRGBFromChatColor;
 
 /**
  * for lazy access due the soft depend
@@ -41,38 +38,5 @@ class ModelEngineAccessor {
             }
         }
         return ret;
-    }
-
-    public static void setGlowing(UUID ownerId, ChatColor color) {
-        ModeledEntity modeledEntity = ModelEngineAPI.getModeledEntity(ownerId);
-        if (modeledEntity == null) {
-            return;
-        }
-        int rgb = getRGBFromChatColor(color);
-        for (ActiveModel model : modeledEntity.getModels().values()) {
-            model.setGlowColor(rgb); // NOTE: ME seems to use ItemDisplay for glowing not scoreboard
-            model.setGlowing(true);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    public static void removeGlowing(UUID ownerId) {
-        ModeledEntity modeledEntity = ModelEngineAPI.getModeledEntity(ownerId);
-        if (modeledEntity == null) {
-            return;
-        }
-        for (ActiveModel model : modeledEntity.getModels().values()) {
-            ModelRenderer render = model.getModelRenderer();
-            RenderQueues<Object> displayRenderer = render instanceof RenderQueues
-                    ? ((RenderQueues<Object>) render)
-                    : null;
-            Map<String, Object> rendered = displayRenderer != null ? displayRenderer.getRendered() : Collections.emptyMap();
-            for (Map.Entry<String, Object> pair : rendered.entrySet()) {
-                Object value = pair.getValue();
-                DisplayRenderer.Bone bone = value instanceof DisplayRenderer.Bone ? ((DisplayRendererImpl.BoneImpl) value) : null;
-                if (bone == null) continue;
-                UUID uuid = bone.getUuid();
-            }
-        }
     }
 }
